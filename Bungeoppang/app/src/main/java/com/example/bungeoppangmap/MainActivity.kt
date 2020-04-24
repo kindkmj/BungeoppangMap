f88b49481773ee6b6acd055f668bb5e48aa38a97
kindkmj <41276448+kindkmj@users.noreply.github.com>
2020-04-24 22:36:45 +0900

    1.01
    카카오셋팅 하지만 안됨 ㅜ

--- Bungeoppang/app/src/main/java/com/example/bungeoppangmap/MainActivity.kt
+++ Bungeoppang/app/src/main/java/com/example/bungeoppangmap/MainActivity.kt
@@ -1,12 +1,58 @@
 package com.example.bungeoppangmap
 
-import android.support.v7.app.AppCompatActivity
+//import sun.jvm.hotspot.utilities.IntArray
+
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Base64
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import net.daum.mf.map.api.MapView
+//import sun.jvm.hotspot.utilities.IntArray
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+
 
 class MainActivity : AppCompatActivity() {
+    private fun getHashKey() {
+        var packageInfo: PackageInfo? = null
+        try {
+            packageInfo =
+                packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
+        }
+        if (packageInfo == null) Log.e("KeyHash", "KeyHash:null")
+        for (signature in packageInfo!!.signatures) {
+            try {
+                val md = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                Log.d(
+                    "KeyHash",
+                    Base64.encodeToString(md.digest(), Base64.DEFAULT)
+                )
+            } catch (e: NoSuchAlgorithmException) {
+                Log.e(
+                    "KeyHash",
+                    "Unable to get MessageDigest. signature=$signature",
+                    e
+                )
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val mapView = MapView(this);
+        mapView.setDaumMapApiKey("6b3f298237486da46949cf5639f84ffa")
+        val mapViewContainer = map_view;
+        mapViewContainer.addView(mapView);
+
+
+        getHashKey();
+
     }
 }
